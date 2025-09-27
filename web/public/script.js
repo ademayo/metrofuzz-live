4f4d04a0cf2daddf851334be3a457b66706d9d5d
Gus deMayo <gus.demayo@gmail.com>
2025-09-26 23:58:31 -0500

    Layout, Style & LiquidSoap Fixes

--- web/public/script.js
+++ web/public/script.js
@@ -1,3 +1,12 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+
+if (menuToggle && nav) {
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+}
+
 const trackInput = document.getElementById('track-input');
 const trackIdInput = document.getElementById('track-id');
 const suggestionsBox = document.getElementById('suggestions');
